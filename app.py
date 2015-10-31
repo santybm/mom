6d51fc7ee647b220a303c9a78a20b25672cb848c
Santiago Beltran <sbeltran@bu.edu>
2015-10-31 15:28:04 -0400

    first init'

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World"
+
+@app.route('/Inna')
+def mei():
+    return "Hello Inna"
+
+@app.route('/kristel')
+def mek():
+    return "Hello Kristel"
+
+
+if __name__ == '__main__':
+    app.run();

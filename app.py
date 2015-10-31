3e3cd1c00ad63293a4970f41fbd55e1e07214536
Santiago Beltran <sbeltran@bu.edu>
2015-10-31 17:06:15 -0400

    login stuff

--- app.py
+++ app.py
@@ -1,19 +1,65 @@
-from flask import Flask
+from flask import Flask, session
 app = Flask(__name__)
+
+import settings_local
+from parse_rest.connection import SessionToken, register
+from parse_rest.user import User
+import process_user
+
+
+register(settings_local.APPLICATION_ID, settings_local.REST_API_KEY)
 
 
 @app.route('/')
 def hello():
     return "Hello World"
 
-@app.route('/login')
-def mei():
-    return "Hello Inna"
 
-@app.route('/kristel')
-def mek():
-    return "Hello Kristel"
+### USER ROUTES ###
+
+@app.route('/register')
+def uregister():
+    username = "demo1"
+    password = "abcd123"
+    u = process_user.signup(username, password)
+    session['token'] = u
+
+    return u.username
+
+
+@app.route('/login')
+def ulogin():
+    username = 'demo'
+    password = "abcd123"
+    u = process_user.login(username, password)
+    session['token'] = u
+
+
+    return u.objectId
+
+@app.route('/logout')
+def ulogout():
+    process_user.logout()
+    session['token'] = None
+    return "Logout Successful"
+
+
+### GROCERY CART ###
+
+@app.route('/addToCart')
+def addCart():
+
+    #Is user logged in?
+    if session['token']:
+        return "User Logged in"
+    else:
+        return "Not logged in"
+
+
+
+
 
 
 if __name__ == '__main__':
+    app.secret_key = 'A0Z=-(0a-/dfhg$%##@ew4rfwc[}{}>#@$>:SKF$%!'
     app.run();
--- process_user.py
+++ process_user.py
@@ -1,8 +1,27 @@
 from parse_rest.user import User
+import settings_local
+
+from parse_rest.connection import SessionToken, register
+register(settings_local.APPLICATION_ID, settings_local.REST_API_KEY)
+
 
 def signup(username, password):
-    u = User.signup(username, password)
+    try:
+        u = User.signup(username, password)
+    except:
+        return "User Already Exists"
+    return u.sessionToken.token
 
 
 def login(username, password):
-    u = User.login(username, password)
+    try:
+        u = User.login(username, password)
+    except:
+        return "Error: User login failed"
+    return u.sessionToken.token
+
+def logout():
+    try:
+        User.logout_user()
+    except:
+        return "Error"
